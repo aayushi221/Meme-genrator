07e7f27371c151c8b49f79c20f0aab987ec49d36
Aayushi Pandey <aayushipandey221@gmail.com>
2021-10-25 21:09:36 +0530

    Add files via upload

---
+++ day22.js
@@ -0,0 +1,29 @@
+
+import React, {useState, useEffect} from 'react';
+import Template from './templates';
+import Meme from './Meme';
+import './day22.css';
+
+
+function Day22(){
+    const [templates, setTemplates] = useState([]);
+    const [meme, setMeme]= useState(null);
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+        .then((res) => res.json())
+        .then((data) => {
+            setTemplates(data.data.memes);
+        });
+    }, []);
+    return (
+        <div className="Appy ">
+            <h1>Meme Generator</h1>
+            {meme === null ? (
+            <Template templates={templates} setMeme = {setMeme} />
+                ) : ( 
+                <Meme meme= {meme} setMeme={setMeme}/>
+                )} 
+        </div>
+    )
+}
+export default Day22; 
